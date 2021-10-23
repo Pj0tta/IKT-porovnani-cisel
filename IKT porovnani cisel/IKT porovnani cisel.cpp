845db941e199781ece27847e1c7184279a40cb59
Petr Habel <petrhabel04@gmail.com>
2021-10-23 23:09:58 +0200

    Přidejte soubory projektu.

---
+++ IKT porovnani cisel/IKT porovnani cisel.cpp
@@ -0,0 +1,18 @@
+﻿// IKT porovnani cisel.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	int x = 19;
+	int y = 89;
+	printf("Cislo %d ", x);
+	if (x > y) {
+		printf("je vetsi nez %d ", y);
+	}
+	else {
+		printf("je mensi nez %d ", y);
+	}
+}
+
